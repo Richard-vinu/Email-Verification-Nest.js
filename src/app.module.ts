0f2e4a9cce33439cdcbbbd9980f21a2d35247de8
Richard-vinu <richardnova1999@gmail.com>
2023-04-25 11:01:31 +0530

    user-registration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
 
+import { UsersModule } from './users';
+import { AuthModule } from './auth';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(<string>process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),   
-
-    UserModule
-  ],
-  // controllers: [AppController],
-  // providers: [AppService],
+  imports: [TypeOrmModule.forRoot(), UsersModule, AuthModule],
 })
 export class AppModule {}
--- z.js
+++
@@ -1,9 +0,0 @@
-const target = { a: 1, b: 2 };
-const source = { b: 4, c: 5 };
-
-const returnedTarget = Object.assign(target, source);
-
-//console.log(target);
-//console.log(source);
-
-console.log(returnedTarget);
